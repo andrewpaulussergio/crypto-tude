35e2937c52bcbd1a2145f615ca1efed0940212a6
andrew-paul <andrewpaulussergio@radsoftsystem.com>
2025-10-05 19:14:24 +0700

    05: Utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// function to format currency in USD
+export const formatCurrency = (amount: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+}
+
+// function to format large numbers with suffixes (Million, Billion)
+export const formatMarketCap = (cap: number) => {
+  if(cap >= 1_000_000_000) {
+    return `${(cap / 1_000_000_000).toFixed(2)}B`;
+  }
+  if(cap >= 1_000_000) {
+    return `${(cap / 1_000_000).toFixed(2)}M`;
+  }
+  return cap.toString();
+}
